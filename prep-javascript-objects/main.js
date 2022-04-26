2f51222357af226d3dc876f293526b51044ce12b
Francis Manalang <francismanalang7@gmail.com>
2022-04-26 23:51:37 +0000

    Completed prep-javascript-objects exercise.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,14 @@
+var person = {
+  firstName: 'Francis',
+  lastName: 'Manalang',
+  hobby: 'Weightlifting',
+  job: 'Pottery',
+  previousJob: 'Influencer'
+};
+console.log(person);
+var fullName = person.firstName + ' ' + person.lastName;
+console.log("The person's name is:", fullName);
+
+console.log("The person's current job is:", person.job);
+console.log("The person's previous job is:", person.previousJob);
+console.log('The complete person object:', person);
